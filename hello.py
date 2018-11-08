1074e939c23ac8cc7b679863d0c19e774c0b59e9
lanzhiwang <hzhilamp@163.com>
2018-11-08 18:25:18 +0800

    实现基本功能

---
+++ hello.py
@@ -0,0 +1,39 @@
+from datetime import datetime
+
+from flask import Flask
+from flask import current_app, g, request, session
+# from flask import before_first_request, before_request, after_request, teardown_request
+from flask import make_response, render_template
+from flask import redirect
+from flask import abort
+
+from flask_script import Manager
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+
+app = Flask(__name__)
+
+manager = Manager(app)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', current_time=datetime.utcnow())
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
+
+
+if __name__ == '__main__':
+    manager.run()
