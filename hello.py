98b5deb28b0ab00ba299d7a9a8b8008c80af4488
lanzhiwang <hzhilamp@163.com>
2018-11-08 20:13:31 +0800

    增加表单功能

--- hello.py
+++ hello.py
@@ -5,17 +5,29 @@ from flask import current_app, g, request, session
 # from flask import before_first_request, before_request, after_request, teardown_request
 from flask import make_response, render_template
 from flask import redirect
+from flask import url_for
 from flask import abort
+from flask import flash
 
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
 
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -26,9 +38,16 @@ def internal_server_error(e):
     return render_template('500.html'), 500
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'), current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
@@ -37,3 +56,7 @@ def user(name):
 
 if __name__ == '__main__':
     manager.run()
+# RZX_HOST = "192.168.100.38"
+# RZX_NAME = "data"
+# RZX_PASSWORD = "surfilter1218"
+# RZX_DB = "db_hawkeye_app"
